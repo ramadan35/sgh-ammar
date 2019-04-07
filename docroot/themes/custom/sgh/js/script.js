fcbee2549429aca04af6c94dbff8044afb4467b7
aqala <ammar.qala@gmail.com>
2019-04-07 18:00:46 +0300

    Comments

--- docroot/themes/custom/sgh/js/script.js
+++ docroot/themes/custom/sgh/js/script.js
@@ -8,6 +8,12 @@
 
     Drupal.behaviors.sgh = {
         attach: function (context) {
+            
+            if ($(window).width() > 460) {
+                var number = $('body .hero_slider #block-slidertextoverlay p').html();
+                $('body .hero_slider #block-slidertextoverlay p').wrap('<a href="tel:' + number + '">' + number + '</a>');
+            }
+            
             // Vartheme subtheme JavaScript behaviors goes here.
             $('.path-frontpage #block-views-block-events-block-1 .field--name-field-date .field--item:last-child , #views-bootstrap-events-block-2 .field--name-field-date .field--item:last-child , .page-node-type-events .field--name-field-date .field--item:last-child , .view-offers-promotions .field--name-field-date .field--item:last-child , .page-node-type-offers-promotions .field--name-field-date .field--item:last-child , .page-node-type-branches #views-bootstrap-events-block-3 .field--name-field-date .field--item:last-child', context).prepend('<div class="field--label">End date</div>');
             $('.path-frontpage #block-views-block-events-block-1 .field--name-field-date .field--item:first-child , #views-bootstrap-events-block-2 .field--name-field-date .field--item:first-child , .page-node-type-events .field--name-field-date .field--item:first-child , .view-offers-promotions .field--name-field-date .field--item:first-child , .page-node-type-offers-promotions .field--name-field-date .field--item:first-child , .page-node-type-branches #views-bootstrap-events-block-3 .field--name-field-date .field--item:first-child', context).prepend('<div class="field--label">Start date</div>');
